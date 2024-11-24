05567da7ce9917f79254db109d35bc8bb0d8c1fa
Deepak Jha <jhakumardeepakk@gmail.com>
2024-11-25 02:23:43 +0530

    Basic_Programming_practice

---
+++ Hello World.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char sentence[100];
+    scanf("%[^\n]s",sentence);
+    printf("Hello, World!\n");
+  	printf("%s",sentence);   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
